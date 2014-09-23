480dc46fe042590ec946372e1d8e65ec8831c34d
Abraham R Burnett <abeburnett@gmail.com>
2014-09-23 14:53:41 -0700

    finish through 13

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
-    render text: params[:post].inspect
+    @post = Post.new(params[:post].permit(:title, :text))
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 
